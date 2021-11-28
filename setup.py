67a32fcbedcde79c35a72d7984a14482485faca1
AbdullahSaquib <abdullahsaquib@gmail.com>
2021-11-28 16:02:44 +0530

    Added packages using find_packages()

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
 setup(name='fuzzy_compare',
-    version='1.0',
+    version='1.1',
     author='Abdullah Saquib',
     author_email='abdullahsaquib@gmail.com',
+    packages=find_packages(),
     description='The package includes a algorithm to calculate similarity between two strings',
     long_description=long_description,
     long_description_content_type="text/markdown",
